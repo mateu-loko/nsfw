4d21d925943849a98e5f3f97825168e59f0bba2e
matheus <76986068+mateu-loko@users.noreply.github.com>
2021-02-18 22:01:13 -0300

    Update help.js

--- database/menu/help.js
+++ database/menu/help.js
@@ -1,48 +1,129 @@
-const help = (prefix, ownerBot, botName) => {
+const help = (prefix, botName, mateu) => {
         return `
 「 *${botName}* 」
 
-◪ *INFO*
   ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${ownerBot}
-◪ *ABOUT*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
-◪ *MENU*
-  │
-  ├─ ❏ ${prefix}logomakermenu
-  ├─ ❏ ${prefix}imagemakermenu
-  ├─ ❏ ${prefix}stickermakermenu
-  ├─ ❏ ${prefix}searchmenu
-  ├─ ❏ ${prefix}educationmenu
-  ├─ ❏ ${prefix}kerangmenu
-  ├─ ❏ ${prefix}downloadermenu
-  ├─ ❏ ${prefix}mememenu
-  ├─ ❏ ${prefix}groupmenu
-  ├─ ❏ ${prefix}soundmenu
-  ├─ ❏ ${prefix}musicmenu
-  ├─ ❏ ${prefix}islammenu
-  ├─ ❏ ${prefix}stalkmenu
-  ├─ ❏ ${prefix}wibumenu
-  ├─ ❏ ${prefix}18+menu
-  ├─ ❏ ${prefix}funmenu
-  ├─ ❏ ${prefix}todmenu
-  ├─ ❏ ${prefix}informationmenu
-  ├─ ❏ ${prefix}stayonscreenmenu
-  ├─ ❏ ${prefix}ownermenu
-  └─ ❏ ${prefix}othermenu
-◪ *OTHER*
-  │
-  ├─ ❏ ${prefix}send
-  ├─ ❏ ${prefix}wame
-  ├─ ❏ ${prefix}virtex
-  ├─ ❏ ${prefix}qrcode
-  ├─ ❏ ${prefix}timer
-  ├─ ❏ ${prefix}fml
-  └─ ❏ ${prefix}fml2`
+  ❏ Criador: ${mateu}
+╔═══════════════════
+║        🌹𝐁𝐎𝐓🌹
+╚═══════════════════
+
+➸ Status: *「 Online 」*
+
+       • ──── ✾ ──── •
+       *FIGURINHAS*【✔】
+       • ──── ✾ ──── •
+      
+➸ Comando : *${prefix}sticker* ou *${prefix}stiker*
+➸ útil em : converter imagem/gif/vídeo em adesivo
+➸ uso : responder imagem/gif/video ou enviar imagem/gif/video com legenda\n
+➸ Comando : *${prefix}toimg*
+➸ útil em : converter adesivo em imagem
+➸ uso : adesivo de resposta\n
+➸ Comando : *${prefix}tsticker* ou *${prefix}tstiker*
+➸ útil em : converter texto em adesivo
+➸ uso : *${prefix}tsticker seu texto aqui*\n
+
+       • ─── ✾ ─── •
+       *MEMES*【✔】
+       • ─── ✾ ─── •
+      
+➸ Comando : *${prefix}meme*
+➸ útil em : mandar imagens aleatórias de meme [inglês]
+➸ uso : basta emviar o comando\n
+➸ Comando : *${prefix}memeindo*
+➸ útil em : mandar imagens aleatórias de meme [indo]
+➸ uso : basta enviar o comando
+
+       • ──── ✾ ──── •
+       *OUTROS...*【✔】
+       • ──── ✾ ──── •
+
+➸ Comando : *${prefix}play*
+➸ útil em : baixar músicas do youtube
+➸ uso : basta enviar o comando\n
+➸ Nota : Você precisa por o nome do video certinho pra ser encontrado\n
+➸ Comando : *${prefix}joox*
+➸ útil em : baixar músicas
+➸ uso : basta enviar o comando\n
+➸ Nota : Você precisa por o nome da música certinho pra ser encontrada\n
+➸ Comando : *${prefix}nsfw*
+➸ útil em : ativar o modo hentai
+➸ uso : basta enviar o comando nsfw 1 para ativar, nsfw 0 para desativar\n
+➸ Nota : Você precisa ser administrador do grupo\n
+➸ Comando : *${prefix}nsfwneko*
+➸ útil em : mandar imagens aleatórias de hentai neko
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}hentai*
+➸ útil em : mandar imagens aleatórias de hentai
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}gtts*
+➸ útil em : converter texto em fala/áudio
+➸ uso : *${prefix}gtts [cc] [text]*\nexemplo : *${prefix}gtts ja Onii-chan*\n
+➸ Comando : *${prefix}loli*
+➸ útil em : mandar imagens aleatórias de loli
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}neko*
+➸ útil em : mandar imagens aleatórias de neko
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}url2img*
+➸ útil em : tirar screenshots da web
+➸ uso : *${prefix}url2img [tipe] [url]*\n
+➸ Comando : *${prefix}ocr*
+➸ útil em : pegar o texto da foto e lhe enviar
+➸ uso : responder imagem ou enviar mensagem com legenda\n
+➸ Comando : *${prefix}wait*
+➸ útil em : pesquisar sobre o anime por imagem [ Que anime é este/que ]
+➸ uso : responder imagem ou enviar imagem com legenda\n
+➸ Comando : *${prefix}setprefix*
+➸ útil em : alterar o prefixo do bot
+➸ uso : *${prefix}setprefix [texto|opcional]*\nexemplo : *${prefix}setprefix ?*
+➸ Nota : Usado somente pelo proprietário do bot\n
+
+       • ─── ✾ ─── •
+       *GRUPO*【✔】
+       • ─── ✾ ─── •
+      
+➸ Comando : *${prefix}linkgroup*
+➸ útil em : enviar o link do grupo
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}tagall*
+➸ útil em : marcar todos os membros do grupo, incluindo administradores
+➸ uso : basta enviar o comando\n
+➸ Nota : Você precisa ser administrador do grupo\n
+➸ Comando : *${prefix}add*
+➸ útil em : adicionar membro ao grupo
+➸ uso : *${prefix}add 5585xxxxx*\n
+➸ Nota : o bot precisa ser admin!\n
+➸ Comando : *${prefix}kick*
+➸ útil em : remover membros do grupo
+➸ uso : *${prefix}kick e o @da pessoa*\n
+➸ Nota : Você precisa ser admin e o bot também
+➸ Comando : *${prefix}promote*
+➸ útil em : tornar membro do grupo um administrador
+➸ uso : *${prefix}promote e o @da pessoa*\n
+➸ Nota : Você precisa ser admin e o bot também
+➸ Comando : *${prefix}demote*
+➸ útil em : tornar o administrador um membro comum
+➸ uso : *${prefix}demote e o @da pessoa*\n
+➸ Nota : Você precisa ser admin e o bot também
+
+       • ────── ✾ ────── •
+       *MENU DO MATEU*【✔】
+       • ────── ✾ ────── •              
+
+╔════════════════════
+  ${prefix}criador
+  DONO DO BOT *🔥mateu🔥*
+  DUVIDAS? 👇
+  WA.me/+1(579)9968046
+╚════════════════════`
 }
+
 exports.help = help
+
+
+
+
+
+
